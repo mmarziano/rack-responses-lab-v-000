996788375a7c3609dd5a6acabfb089bee2cb86f4
Monica Marziano <mmarziano1@gmail.com>
2019-03-12 21:53:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,15 @@ class Application
   def call(env)
     
     resp = Rack::Response.new
-    binding.pry
+     
+    time = Time.now.hour
+    
     if time < 12 
       resp.write "Good Morning!"
     else 
       resp.write "Good Afternoon!"
     end
+    
+    resp.finish
   end 
 end 
